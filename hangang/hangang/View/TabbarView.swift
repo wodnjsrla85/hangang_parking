d5e5a97d936c76eb49d246a24424a969e8db8756
wodnjsrla85 <wodnjsrla85@naver.com>
2025-08-18 17:33:23 +0900

    default swift project file

---
+++ hangang/hangang/View/TabbarView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hangang
+//
+//  Created by 김재원 on 8/18/25.
+//
+
+import SwiftUI
+
+struct TabbarView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TabbarView()
+}
---
+++ hangang/hangang/hangangApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangangApp.swift
+//  hangang
+//
+//  Created by 김재원 on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct hangangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabbarView()
+        }
+    }
+}
