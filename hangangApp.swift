fc00f8801ab5cd5f0bf34474885ac47ea9d769b7
jihokim1 <kig0128@naver.com>
2025-08-20 18:26:19 +0900

    버스킹페이지

---
+++ hangangApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangangApp.swift
+//  hangang
+//
+//  Created by 김지호 on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct hangangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
