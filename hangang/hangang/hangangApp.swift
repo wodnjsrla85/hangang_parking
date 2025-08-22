6d1ee4e6d86dbc2360e939ef5ec626fc74c39f2d
jihokim1 <kig0128@naver.com>
2025-08-22 12:01:58 +0900

    22

--- hangang/hangang/hangangApp.swift
+++ hangang/hangang/hangangApp.swift
@@ -2,16 +2,19 @@
 //  hangangApp.swift
 //  hangang
 //
-//  Created by 김지호 on 8/19/25.
+//  Created by 김재원 on 8/18/25.
 //
 
 import SwiftUI
 
 @main
 struct hangangApp: App {
+//    @StateObject private var userManager = UserManager.shared()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BuskingView()
+                .environmentObject(UserManager.shared)
         }
     }
 }
